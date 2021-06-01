55ac310de784db1a93c099ecf029eba63ddfd6ae
Jeevansh12 <76777313+Jeevansh12@users.noreply.github.com>
2021-06-01 13:41:20 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD_nVcokkX4cAm8QpCR5vsVUA7AQIkh6cM",
+    authDomain: "kwitter-bd2c2.firebaseapp.com",
+    projectId: "kwitter-bd2c2",
+    storageBucket: "kwitter-bd2c2.appspot.com",
+    messagingSenderId: "942489041795",
+    appId: "1:942489041795:web:6cb0b0657cb37bfb59d4b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
